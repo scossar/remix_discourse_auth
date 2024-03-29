10c1b94dd22bfc891c23fe4f7e67a384f77b6b46
scossar <scossar3@gmail.com>
2024-03-28 20:12:27 -0700

    remove test links from index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Discourse Auth for Remix" },
+    { name: "description", content: "Discourse auth for Remix" },
   ];
 };
 
@@ -12,15 +11,6 @@ export default function Index() {
   return (
     <div className="p-3">
       <h1 className="text-3xl">Discourse Auth Test</h1>
-      <div className="flex flex-col">
-        <Link className="py-3" to="/login">
-          Login through Discourse (/login)
-        </Link>
-        <Link className="py-3" to="/login?sso=foo&sig=bar">
-          Visiting this link will destroy the nonce session
-          (/login?sso=foo&sig=bar)
-        </Link>
-      </div>
     </div>
   );
 }
